fab5f4f43ceac53f233da5be2c64d47bbb2c20da
adr-enal-in <holla@adr.enal.in>
2013-03-10 18:37:14 -0700

    Updated crashlytics

---
+++ Famish/Config/Config.h
@@ -0,0 +1,12 @@
+//
+//  COnfig.h
+//  Famish
+//
+//  Created by Francisco Flores on 3/10/13.
+//  Copyright (c) 2013 adr.enal.in Groupe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define kFontHeader "Avenir-Heavy"
+#define kFontBody   "Avenir-Medium"
